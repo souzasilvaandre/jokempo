c79f2a4a56da1d2063830b370ae5cecdacfa21ce
André Luis <andre_souzasilva@hotmail.com>
2023-06-13 22:09:38 -0300

    final

--- lib/main.dart
+++ lib/main.dart
@@ -8,3 +8,16 @@ void main(List<String> args) {
 
   ));
 }
+class Jogo extends StatefulWidget {
+  const Jogo({super.key});
+
+  @override
+  State<Jogo> createState() => _JogoState();
+}
+
+class _JogoState extends State<Jogo> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
